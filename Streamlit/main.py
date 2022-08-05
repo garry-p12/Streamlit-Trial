3ad6aebaf14e6aefc14ceca9fafb6c2903a7f409
garry-p12 <105554007+garry-p12@users.noreply.github.com>
2022-08-05 12:55:13 +0530

    Add files via upload

---
+++ Streamlit/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+
+# In[2]:
+
+
+header = st.container()
+
+
+# In[ ]:
+
+
+with header:
+    st.title('My streamlit deployment!')
+
